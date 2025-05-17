d156f7a4c786b4c4475d0c34c18e9a7bce7f872b
AnujNaval <145321158+anujnaval13@users.noreply.github.com>
2025-05-18 00:08:46 +0530

    Login Function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -58,7 +58,53 @@ const register = async (req, res) => {
 }
 
 const login = async(req, res) => {
-    
+    try {
+        const {role, email, password} = req.body;
+
+        // Basic Validation
+        if(!email || !role || !password){
+            return res.status(400).json({message: "Please enter Email, Password and Role!"});
+        }
+
+        // Check if User with Email Exists
+        const user = User.findOne({email});
+        if(!user){
+            return res.status(400).json({message: "Invalid email or password!"});
+        }
+
+        // Compare Password with stored hashed password
+        const isMatch = bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({message: "Invalid email or passord!"});
+        }
+
+        // Check the Role Match
+        if(user.role != role){
+            return res.status(400).json({message: `Access denied for ${role}`});
+        }
+
+        // Generate Token 
+        const token = jwt.sign(
+            {id: user._id, role: user.role},
+            process.env.JWT_SECRET,
+            {expiresIn: "1h"},
+        );
+
+        // Response 
+        res.status(200).json({
+            message: "Login Successful!",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+            }
+        });
+    } catch(error) {
+        console.error('Login error:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
 }
 
 const getMe = async(req, res) => {
