60452500989c1d9a89ec50e2f2dedd60199cf43c
AnujNaval <145321158+anujnaval13@users.noreply.github.com>
2025-05-17 23:52:27 +0530

    File Structure of Backend and register function

---
+++ server/controllers/authController.js
@@ -0,0 +1,72 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const register = async (req, res) => {
+    try {
+        const {name, email, phone, role, password} = req.body;
+
+        // Basic Validation
+        if(!name || !email || !phone || !role || !password){
+            return res.status(400).json({message: "Please enter all required fiels!"});
+        }
+
+        // Check if User Already Exists
+        const existingUser = User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({message: "User already exists with this email!"});
+        }
+
+        // Hash Password
+        const salt = await bcrypt.getSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Create New User
+        const user = new User({
+            name, 
+            email, 
+            password: hashedPassword,
+            role, 
+            phone,
+        });
+
+        await user.save();
+
+        // Create JWT Token
+        const token = jwt.sign(
+            {id: user._id, role: user.role},
+            process.env.JWT_SECRET,
+            {expiresIn: "1h"}
+        );
+
+        // Send Response
+        res.status(201).json({
+            message: "User Successfully Registered!",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                phone: user.phone,
+            }
+        });
+    } catch(error){
+        console.error('Register error:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+}
+
+const login = async(req, res) => {
+    
+}
+
+const getMe = async(req, res) => {
+
+}
+
+module.exports = {
+    register,
+    login,
+    getMe,
+};
